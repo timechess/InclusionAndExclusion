1d11636b4e4b7781471e0485924b8877ac23f777
gnawyymmij <wangyang@westlake.edu.cn>
2024-08-26 18:40:48 +0800

    Completed proof of FinIneter.lean (#5)
    * proved Multiset.FinInter (#4)
    
    * proved FinInter_Fin & polished previous proofs
    
    * proved Multiset.eq_FinInter (#5)
    
    * Proved eq_FinInter0 (#6)
    
    * fixed a warning & add (Waited to be written...) blank in content.tex
    
    * proved char_fun_FinInter
    
    * modified Auxiliary.lean to teacher's version
    
    * proved card_eq_sum_char_fun
    
    * fixed tex
    
    * git push origin master (#7)
    
    * newadd (#8)
    
    * fix
    
    * finished content of eq_FinInter0 and proved Principle_of_Inclusion_Exclusion (#9)
    
    * proved mul_expand0
    
    * added natural language proof of mul_expand0
    
    * fixed tex
    
    ---------
    
    Co-authored-by: yukai233 <107670931+yukai233@users.noreply.github.com>
    Co-authored-by: LikC1606 <3100378030@qq.com>
    Co-authored-by: Tangent1024 <meike@westlake.edu.cn>
    Co-authored-by: timechess <tyc221@ruc.edu.cn>

--- InclusionAndExclusion/FinInter.lean
+++ InclusionAndExclusion/FinInter.lean
@@ -21,31 +21,70 @@ lemma List.eq_FinInter {α β : Type*} [DecidableEq α] [Fintype β] (A : β →
   | x1 :: x2 :: xs => (by
     unfold List.FinInter
     simp only [Finset.mem_inter, mem_cons, forall_eq_or_imp, and_congr_right_iff]
-    intro x a
+    intro x
     have := List.eq_FinInter A (x2 :: xs) (cons_ne_nil x2 _)
     simp only [this, mem_cons, forall_eq_or_imp]
-
+    simp only [implies_true]
   )
 
 /-- We say the intersection of several finite sets does not depend on the order in which who and who intersect first. Therefore, we introduce the definition of multiset. Given a function A, we define a new function from a multiset to the intersection of finite sets whose index is in the multiset -/
 def Multiset.FinInter {α β : Type*} [DecidableEq α] [Fintype β] (A : β → Finset α) : Multiset β → Finset α :=
   Quot.lift (α := List β) (β := Finset α) (List.FinInter A) (by
-    sorry
-    )
+    intro a b hab
+    simp [Setoid.r] at hab
+    ext x
+    by_cases ept:a≠ []
+    have :b≠ []:=by
+      contrapose! ept
+      rw[ept] at hab
+      exact Eq.symm (List.Perm.nil_eq (id (List.Perm.symm hab)))
+    rw[List.eq_FinInter _ _ this]
+    rw[List.eq_FinInter _ _ ept]
+    constructor
+    · intro ha aa bb
+      have hyp: aa ∈ a := by exact (List.Perm.mem_iff (id (List.Perm.symm hab))).mp bb
+      have hyp' : x ∈ A aa := by exact ha aa hyp
+      exact hyp'
+    · intro ha aa bb
+      have hyp: aa ∈ b := by exact (List.Perm.mem_iff hab).mp bb
+      exact ha aa hyp
+    simp only [ne_eq, not_not] at ept
+    rw[ept]
+    have : b = [] := by rw[ept] at hab;exact Eq.symm (List.Perm.nil_eq hab)
+    rw[this]
+  )
 
 /-- We prove the lemma 'List.eq_FinInter' to be still true in the multiset case -/
 lemma Multiset.eq_FinInter {α β : Type*} [DecidableEq α] [Fintype β] (A : β → Finset α) (M : Multiset β) (h : M ≠ ∅) : ∀ x : α, x ∈ M.FinInter A ↔ ∀ m ∈ M, x ∈ A m := by
-  sorry
-
+  intro x
+  have : M.FinInter A = M.toList.FinInter A := by
+    unfold Multiset.FinInter
+    have : M.toList = M := by simp
+    nth_rw 1 [← this]
+    rfl
+  rw [this]
+  rw [List.eq_FinInter]
+  simp
+  simpa
 /-- Given a finite index set (@Finset.univ β _), we define FinInter₀ to be the intersection of all finite sets whose index's type is β -/
 def FinInter₀ {α β : Type*} [DecidableEq α] [Fintype β] [Nonempty β] (A : β → Finset α) : Finset α := Multiset.FinInter A (Finset.univ).1
 
 /-- Same as above, we prove the lemma 'List.eq_FinInter' to be still true in the whole case -/
 lemma eq_FinInter₀ {α β : Type*} [DecidableEq α] [Fintype β] [h : Nonempty β] (A : β → Finset α) :
   FinInter₀ A = ⋂ (i : β), (A i : Set α) := by
-  sorry
+  unfold FinInter₀
+  ext x
+  simp
+  rw[Multiset.eq_FinInter]
+  · simp
+  · have h1:∀ t:β,t∈ Finset.univ.val :=by exact fun t => Finset.mem_univ_val t
+    intro h2
+    rw[h2]at h1
+    simp only [Multiset.empty_eq_zero, Multiset.not_mem_zero, forall_const] at h1
 
 /-- We show that the intersection of finite number of finite sets is still a finite set -/
 instance FinInter_Fin {α β : Type*} [DecidableEq α] [Fintype β] [Nonempty β] (A : β → Finset α) :
   Fintype (⋂ (i : β), (A i : Set α)) := by
-  sorry
+  rw [← eq_FinInter₀]
+  simp only [Finset.coe_sort_coe]
+  exact FinsetCoe.fintype (FinInter₀ A)
